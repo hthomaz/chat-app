d2df9aba6beddccf36edea8e512dfa76a5ca2f40
hthomaz <heitor_thomaz@hotmail.com>
2022-01-02 14:43:32 -0300

    Adicionado arquivo .env

--- go.mod
+++ go.mod
@@ -3,4 +3,7 @@ module heitor
 
 go 1.14
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/joho/godotenv v1.4.0
+)
--- main.go
+++ main.go
@@ -3,12 +3,21 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
 	http.Handle("/", http.FileServer(http.Dir("./public")))
 	log.Print("Server starting at localhost:4444")
-	if err := http.ListenAndServe(":4444", nil); err != nil {
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Fatal(err)
 	}
 }
