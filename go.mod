13924bf74d2894236dfd9db838f5f82263ee7d05
hthomaz <heitor_thomaz@hotmail.com>
2022-01-02 17:05:09 -0300

    Regras de negocio para backend finalizadas

--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ go 1.14
 require (
 	github.com/go-redis/redis v6.15.9+incompatible
 	github.com/gorilla/mux v1.8.0
+	github.com/gorilla/websocket v1.4.2
 	github.com/joho/godotenv v1.4.0
 )
--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"encoding/json"
+	"io"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/go-redis/redis"
+	"github.com/gorilla/websocket"
 	"github.com/joho/godotenv"
 )
 
@@ -13,23 +16,118 @@ var (
 	rdb *redis.Client
 )
 
+type ChatMessage struct {
+	Username string `json:"username"`
+	Text     string `json:"text`
+}
+
+var clients = make(map[*websocket.Conn]bool)
+var broadcaster = make(chan ChatMessage)
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
 func main() {
+
+	//Load enviroments data
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
 	redisURL := os.Getenv("REDIS_URL")
+	port := os.Getenv("PORT")
+
 	opt, err := redis.ParseURL(redisURL)
 	if err != nil {
 		panic(err)
 	}
 	rdb = redis.NewClient(opt)
 
-	port := os.Getenv("PORT")
 	http.Handle("/", http.FileServer(http.Dir("./public")))
+	http.HandleFunc("/websocket", handleConnections)
+	go handleMessages()
+
 	log.Print("Server starting at localhost:4444")
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ws.Close()
+	clients[ws] = true
+
+	if rdb.Exists("chats_messages").Val() != 0 {
+		sendPreviousMessages(ws)
+	}
+
+	for {
+		var msg ChatMessage
+
+		err := ws.ReadJSON(&msg)
+		if err != nil {
+			delete(clients, ws)
+			break
+		}
+		broadcaster <- msg
+	}
+}
+
+func sendPreviousMessages(ws *websocket.Conn) {
+	chatMessages, err := rdb.LRange("chat_messages", 0, -1).Result()
+	if err != nil {
+		panic(err)
+	}
+
+	for _, chatMessage := range chatMessages {
+		var msg ChatMessage
+		json.Unmarshal([]byte(chatMessage), &msg)
+		sendMessageToClient(ws, msg)
+	}
+}
+
+func handleMessages() {
+	for {
+		msg := <-broadcaster
+		storeInRedis(msg)
+		sendMessageToClients(msg)
+	}
+}
+
+func storeInRedis(msg ChatMessage) {
+	json, err := json.Marshal(msg)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := rdb.RPush("chat_messages", json).Err(); err != nil {
+		panic(err)
+	}
+}
+
+func sendMessageToClients(msg ChatMessage) {
+	for client := range clients {
+		sendMessageToClient(client, msg)
+	}
+}
+
+func sendMessageToClient(client *websocket.Conn, msg ChatMessage) {
+	err := client.WriteJSON(msg)
+	if err != nil && unsafeError(err) {
+		log.Printf("error :%v", err)
+		client.Close()
+		delete(clients, client)
+	}
+}
+
+// If a message is sent while a client is closing, ignore the error
+func unsafeError(err error) bool {
+	return !websocket.IsCloseError(err, websocket.CloseGoingAway) && err != io.EOF
 }
