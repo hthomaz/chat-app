77ed6b360580ee2e43add4918b48286290d10d83
hthomaz <heitor_thomaz@hotmail.com>
2022-01-02 14:33:43 -0300

    Alteracao para utilizar http.Handler

--- main.go
+++ main.go
@@ -1,18 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-
-	"github.com/gorilla/mux"
 )
 
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, world!")
-	})
+	http.Handle("/", http.FileServer(http.Dir("./public")))
 	log.Print("Server starting at localhost:4444")
-	http.ListenAndServe(":4444", r)
+	if err := http.ListenAndServe(":4444", nil); err != nil {
+		log.Fatal(err)
+	}
 }
